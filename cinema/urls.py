8d1e3faec81b2b41794ec55dee14d32e88a2da4f
PatrickCosta7 <pl.costa@hotmail.com>
2025-03-13 16:33:22 -0300

    change: uptade to adapt to  django file system

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from cinema.views import index 
+from cinema.views import index, movie_page
 
 urlpatterns = [
     path('', index, name='index'),
+    path('filme/<str:movie_id>', movie_page, name='movie_page'),
 ]
--- cinema/views.py
+++ cinema/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main/index.html')
+
+def movie_page(request, movie_id):
+    return render(request, f'main/{movie_id}.html')
